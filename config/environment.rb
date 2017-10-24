75c13be09116e5084c647996762a8bf2db97c0df
Gonzodamus <Dick@TheDickWard.com>
2017-10-24 10:05:07 -0400

    deleteme

--- config/environment.rb
+++ config/environment.rb
@@ -6,5 +6,3 @@ ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/develo
 require_all 'lib'
 require_all 'apps'
 require_all 'db'
-
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
