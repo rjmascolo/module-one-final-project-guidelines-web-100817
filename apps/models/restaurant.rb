f547e2ff3dab3c8d063fc60117a233efcaf2be42
github username <github email address>
2017-10-24 16:40:38 -0400

    Fixed up some helper methods

--- apps/models/restaurant.rb
+++ apps/models/restaurant.rb
@@ -1,11 +1,11 @@
 class Restaurant < ActiveRecord::Base
   has_many :inspections
   has_many :violations, through: :inspections
+
 ########## Ryan's Section ############
   def self.most_sanitary_in_area_by_cuisine(zipcode, cuisine)
     #finds restaurants in zipcode with a cuisine type
     in_area = self.restaurant_type_in_zip(zipcode,cuisine)
-
     # finds the the inspection score of the restaurants last inspection
     #placer data that will keep track of who wone
     winning_score = 1000
@@ -13,20 +13,13 @@ class Restaurant < ActiveRecord::Base
     #check each restaurant to see which one has the lowest score
     in_area.each do |obj|
       #need to find the latest inspection report
-      if obj.inspections.length > 1
-        last_date = "0"
-        obj.inspections.each { |inspec_obj| inspec_obj[:inspection_date] > last_date ? last_date = inspec_obj[:inspection_date] : nil }
-        last_inspection = obj.inspections.where(inspection_date: last_date)
-        if last_inspection[0].score < winning_score
-          winning_score = last_inspection[0].score
-          best_restaurant = obj.name
-        end
-      elsif last_inspection[0].score < winning_score
-        winning_score = last_inspection[0].score
+      last_inspection = obj.most_recent_inspection
+      if last_inspection.score < winning_score
+        winning_score = last_inspection.score
         best_restaurant = obj.name
       end
     end
-    return "#{winning_score} #{best_restaurant}"
+    return "Score:#{winning_score} Restaurant:#{best_restaurant}"
   end
 
   def self.restaurant_type_in_zip(zipcode,cuisine)
@@ -67,8 +60,13 @@ end
 
 
 ## Not Finished, but in progress ##
-def most_recent_inspection
+def most_recent_inspection_date
   self.inspections.maximum(:inspection_date)
+end
+
+def most_recent_inspection
+  last_date = most_recent_inspection_date
+  self.inspections.where(inspection_date:last_date).first
 end
 
 ###  need to update to find most recent inspection - ryan has code for this ###
