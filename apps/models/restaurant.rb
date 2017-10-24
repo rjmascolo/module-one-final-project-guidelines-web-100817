d34989459b3081fe80bf0d7971b00b9a66ede054
Gonzodamus <Dick@TheDickWard.com>
2017-10-24 15:24:13 -0400

    Added methods for address, find restaurant by zipcode and find most sanitary by zipcode.

--- apps/models/restaurant.rb
+++ apps/models/restaurant.rb
@@ -2,4 +2,41 @@ class Restaurant < ActiveRecord::Base
   has_many :inspections
   has_many :violations, through: :inspections
 
+
+
+def address
+ "#{self.building} #{self.street} #{self.zipcode}"
+end
+
+def self.find_latest_inspection_by_name_and_zipcode(name, zipcode)
+  restaurant = Restaurant.find_by(name: name, zipcode: zipcode)
+  if restaurant == nil
+    "No records found. Please check the restaurant name and zipcode and try again."
+  else
+  "The restaurants last inspection was on #{Date.parse(restaurant.inspections[0].inspection_date)}
+    Its score was #{restaurant.inspections[0].score}.
+    Its grade was #{restaurant.inspections[0].grade}."
+  end
+end
+
+def self.find_restaurant_by_zipcode(zipcode)
+  Restaurant.where(zipcode: zipcode)
+end
+
+def self.find_most_sanitary_restaurant_by_zipcode(zipcode)
+  score = 0
+  restaurant_name= ""
+  restaurants_in_zip = find_restaurant_by_zipcode(zipcode)
+  restaurants_in_zip.each do |restaurant|
+    if restaurant.inspections[0].score > score
+      restaurant_name = restaurant
+    end
+  end
+  restaurant_name.name
+end
+
+
+
+
+
 end
