14d473dd8c7afb123aa9a1437006377e2f96125c
github username <github email address>
2017-10-25 13:26:13 -0400

    Last method is finalized

--- apps/models/inspection.rb
+++ apps/models/inspection.rb
@@ -3,4 +3,35 @@ class Inspection < ActiveRecord::Base
   has_many :inspection_violations
   has_many :violations, through: :inspection_violations
 
+  # User case 4: this function returns the top 5 restaurants in the zipcode
+  def self.resturant_info_of_top_five_inspections_by_zipcode(zipcode)
+    array = self.most_recent_inspection_and_minimum_score_in_areacode(zipcode).take(5)
+    array.map { |inspection| inspection.restaurant }
+  end
+
+  #helper method
+  def self.inspections_ordered_by_scores
+    self.order(:score).where("score IS NOT NULL")
+  end
+
+  def self.filter_by_cuisine(cuisine)
+    includes(:restaurant).where(restaurants: {cuisine: cuisine})
+  end
+
+  def self.filter_by_zipcode(zipcode)
+    includes(:restaurant).where(restaurants: {zipcode: zipcode})
+  end
+
+  def self.minimum_score_in_areacode(zipcode)
+    self.filter_by_zipcode(zipcode).inspections_ordered_by_scores
+  end
+
+  def self.most_recent_inspection_and_minimum_score_in_areacode(zipcode)
+    self.minimum_score_in_areacode(zipcode).select {|e| e.most_recent_inspection?}
+  end
+
+  def most_recent_inspection?
+    self.restaurant.most_recent_inspection == self ? true : false
+  end
+
 end
--- apps/models/restaurant.rb
+++ apps/models/restaurant.rb
@@ -4,36 +4,25 @@ class Restaurant < ActiveRecord::Base
 
 ########## Ryan's Section ############
   def self.most_sanitary_in_area_by_cuisine(zipcode, cuisine)
-    #finds restaurants in zipcode with a cuisine type
     in_area = self.restaurant_type_in_zip(zipcode,cuisine)
-    # finds the the inspection score of the restaurants last inspection
-    #placer data that will keep track of who wone
-    winning_score = 1000
-    best_restaurant = ""
-    #check each restaurant to see which one has the lowest score
+    last_inspections = {}
     in_area.each do |obj|
-      #need to find the latest inspection report
-      last_inspection = obj.most_recent_inspection
-      if last_inspection.score < winning_score
-        winning_score = last_inspection.score
-        best_restaurant = obj.name
-      end
+      last_inspect = obj.most_recent_inspection
+      last_inspections[last_inspect.id] = last_inspect.score
     end
-    return "Score:#{winning_score} Restaurant:#{best_restaurant}"
+    restaurant_id = [last_inspections.min_by{|k, v| v}].to_h.keys[0]
+    self.find(restaurant_id)
   end
 
   def self.restaurant_type_in_zip(zipcode,cuisine)
     self.where(zipcode: zipcode).where(cuisine: cuisine)
   end
 
-  def self.more_than_one_inspection?
-    self.all.select { |r| r.inspections.length > 1 }
-  end
-
   def self.most_populated_zip_of_cuisine(cuisine)
     x = self.where(cuisine: cuisine).group(:zipcode).count
     x.sort_by { |name, age| age }
   end
+
 ######### Ryan's section ending ###################
 
 
