057c6ba875884c1925f434be53267910ef0ef36a
github username <github email address>
2017-10-25 19:33:04 -0400

    things done

--- lib/user_story_2.rb
+++ lib/user_story_2.rb
@@ -1,24 +1,43 @@
 #this file is for the user story 2.
 
 def returns_most_sanitary_rest_in_zip_and_cuisine
-  puts "What zipcode are you searching for food in?"
-  zipcode_input = gets.chomp
-  binding.pry
-   if !valid_zipcode?(zipcode_input)
-     puts "That is an invalid zipcode. Please try again"
-     returns_most_sanitary_rest_in_zip_and_cuisine
-   end
-  puts "What type of cuisine would you like?"
-  puts "Below are some options in your neighborhood"
+  zipcode_input = retrive_the_zipcode
+  puts "\nWhat type of cuisine would you like?\n\n"
   cusine_options_in_neighborhood(zipcode_input)
-  cuisine_input = get_input
-  list_of_restaurants = Restaurant.most_sanitary_in_area_by_cuisine(zipcode, cuisine)
-
+  puts "\nAbove are some options in your neighborhood:"
+  cuisine_input = retrive_the_cuisine(zipcode_input)
+  puts return_data_format(Restaurant.most_sanitary_in_area_by_cuisine(zipcode_input, cuisine_input).name)
 end
 
+def retrive_the_cuisine(zipcode)
+  cuisine_input = gets.chomp
+  while !valid_cuisine_type?(cuisine_input, zipcode)
+    puts "That is not a valid cuisine type. Please try again."
+    cuisine_input = gets.chomp
+  end
+  cuisine_input
+end
+
+def valid_cuisine_type? (cuisine, zipcode)
+  Restaurant.cuisine_available_in_zipcode(zipcode).any?{ |word| word.downcase == cuisine.downcase }
+end
+
+def retrive_the_zipcode
+  puts "What zipcode are you searching for food in?"
+  zipcode_input = gets.chomp
+   while !valid_zipcode?(zipcode_input)
+     zipcode_input = gets.chomp
+   end
+   zipcode_input
+end
 
 def valid_zipcode?(zipcode)
-  Restaurant.zipcodes_of_restuarants.any?(zipcode)
+  if zipcode.length != 5
+    puts "Your zipcode is not 6 digits. Please reenter.\n"
+    false
+  else
+    Restaurant.zipcodes_of_restuarants.any?{|word| word == zipcode}
+  end
 end
 
 def cusine_options_in_neighborhood(zipcode)
