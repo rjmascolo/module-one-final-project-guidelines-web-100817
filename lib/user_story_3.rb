281723ea14cca0ee0c64a780a8a0b11244487ba1
Gonzodamus <Dick@TheDickWard.com>
2017-10-26 14:37:25 -0400

    fixes

--- lib/user_story_3.rb
+++ lib/user_story_3.rb
@@ -2,10 +2,7 @@ def user_story_three
   system('clear')
   puts rat
   puts ""
-<<<<<<< HEAD
-  
   Inspection.find_rodents_by_zipcode(gets_rodent_zip)
-
 end
 
 
@@ -16,10 +13,4 @@ def gets_rodent_zip
      zipcode_input = gets.chomp
    end
    zipcode_input
-=======
-  puts "Enter the zip code that you'd like a rodent report for."
-  zipcode = gets.chomp
-  Inspection.find_rodents_by_zipcode(zipcode)
-  return_to_menu_or_exit
->>>>>>> 5b8ca8d42a294844dc7cd1fa4fc8b79029a21fe4
 end
